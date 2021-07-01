2b4bd0b7e2499e58a9affa96db569af1b6f4f460
htbak <hazel.baker-harevy@smoothstack.com>
2021-07-01 00:52:40 -0400

    Made into spring

---
+++ src/test/java/com/ss/eastcoderbank/accountservice/AccountserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.eastcoderbank.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
