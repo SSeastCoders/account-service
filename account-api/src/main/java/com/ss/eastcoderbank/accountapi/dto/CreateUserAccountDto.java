57d06be5ff38eba73fbce1d8779a3c54e2cfbf0b
asernass <alejandro.serna@smoothstack.com>
2021-08-11 17:52:54 -0400

    updated core-library

--- account-api/src/main/java/com/ss/eastcoderbank/accountapi/dto/CreateUserAccountDto.java
+++ account-api/src/main/java/com/ss/eastcoderbank/accountapi/dto/CreateUserAccountDto.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -13,11 +16,16 @@ import java.util.List;
 @AllArgsConstructor
 public class CreateUserAccountDto {
     @NotNull
-    protected AccountType accountType;
+    private AccountType accountType;
 
     @NotNull
-    protected Float balance;
+    @Min(value = 0, message = "Balance can not be negative.")
+    private Float balance;
 
     @NotNull
-    protected List<Integer> usersIds;
+    private List<Integer> usersIds;
+
+    @NotBlank
+    @Size(max = 20, message = "name must be less then 20 characters")
+    private String nickName;
 }
