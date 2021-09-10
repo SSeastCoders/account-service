311056a2c8ceaf94d4165dd2b60084c7c4e72d74
Luis Fernandez <82048688+LuisF111@users.noreply.github.com>
2021-09-10 16:55:15 -0400

    Revert "Java 11 change"

--- account-api/src/test/java/com/ss/eastcoderbank/accountapi/AccountServiceApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.ss.eastcoderbank.accountapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AccountServiceApplicationTests {
-
-}
