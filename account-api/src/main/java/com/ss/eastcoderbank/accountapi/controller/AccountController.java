6ac7e4bfa36988cb56a460ffeccd3b0a269bd702
Hazel Baker-Harvey <84642569+hazelbakerharvey@users.noreply.github.com>
2021-10-27 11:44:27 -0700

    Logging and monitoring

--- account-api/src/main/java/com/ss/eastcoderbank/accountapi/controller/AccountController.java
+++ account-api/src/main/java/com/ss/eastcoderbank/accountapi/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.ss.eastcoderbank.accountapi.dto.CreateUserAccountDto;
 import com.ss.eastcoderbank.accountapi.dto.UpdateAccountDto;
 import com.ss.eastcoderbank.accountapi.service.AccountService;
 import com.ss.eastcoderbank.core.transferdto.AccountDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("api/v1/accounts")
@@ -24,43 +26,56 @@ public class AccountController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createAccount(@Valid @RequestBody CreateUserAccountDto account) {
+        log.trace("POST account endpoint reached...");
         accountService.createAccount(account);
     }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<List<AccountDto>> getAccountByUser(@PathVariable Integer id) {
+        log.trace("Get account/users/id endpoint reached...");
+        log.debug("Endpoint id: {}", id);
         return new ResponseEntity<>(accountService.getAccountsByUser(id), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<String> deactivateAccount(@PathVariable Integer id) {
+        log.trace("DELETE account/id endpoint reached...");
+        log.debug("Endpoint id: {}", id);
         accountService.deactivateAccount(id);
         return new ResponseEntity<>("Account deleted", HttpStatus.NO_CONTENT);
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<AccountDto> getAccount(@PathVariable Integer id) {
+        log.trace("GET account/id endpoint reached...");
+        log.debug("Endpoint id: {}", id);
         return new ResponseEntity<>(accountService.getAccountById(id), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<List<AccountDto>> getAllAccounts() {
+        log.trace("GET account endpoint reached, not paginated...");
         return new ResponseEntity<>(accountService.getAccounts(), HttpStatus.OK);
     }
 
     @GetMapping("/accountsPage")
     public ResponseEntity<Page<AccountDto>> getPaginatedAccounts(@RequestParam(name="page") Integer pageNumber, @RequestParam(name="size") Integer pageSize, @RequestParam(value="asc", required = false) boolean asc, @RequestParam(value = "sort", required = false) String sort, Pageable page) {
+        log.trace("get account endpoint reached...");
+        log.debug("Endpoint pageNumber: {}, pageSize: {}", pageNumber, pageSize);
         return new ResponseEntity<>(accountService.getAccounts(pageNumber, pageSize, asc, sort), HttpStatus.OK);
-        //return accountService.getPaginatedAccounts(pageNumber, pageSize);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.PARTIAL_CONTENT)
     public void updateAccount(@Valid @RequestBody UpdateAccountDto updateAccountDto, @PathVariable Integer id) {
+        log.trace("PUT account endpoint reached...");
+        log.debug("Endpoint id: {}", id);
         accountService.updateAccount(updateAccountDto, id);
     }
 
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
-    public void healthCheck() {}
+    public void healthCheck() {
+        log.trace("HEALTH account endpoint reached...");
+    }
 }
--- account-api/src/main/java/com/ss/eastcoderbank/accountapi/service/AccountService.java
+++ account-api/src/main/java/com/ss/eastcoderbank/accountapi/service/AccountService.java
@@ -15,11 +15,9 @@ import com.ss.eastcoderbank.core.transferdto.AccountDto;
 import com.ss.eastcoderbank.core.transfermapper.AccountMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -42,10 +40,17 @@ public class AccountService {
 
 
     public void createAccount(CreateUserAccountDto payload) {
+
+        log.info("Request for new account...");
+
+        log.info("Mapping request DTO to account object...");
         Account account = accountCreateMapper.mapToEntity(payload);
 
+        log.info("Transforming user ids into user list...");
         List<User> users = new ArrayList<>();
         payload.getUsersIds().forEach(integer -> users.add(userRepository.findById(integer).orElseThrow(UserNotFoundException::new)));
+
+        log.info("Applying default account setting to new account...");
         account.setActiveStatus(true);
         if (account.getBalance() < 0) {
             account.setBalance(0F);
@@ -54,42 +59,62 @@ public class AccountService {
         account.setOpenDate(LocalDate.now());
         account.setUsers(users);
 
+        log.info("Saving new account to database...");
         accountRepository.save(account);
 
     }
 
     public List<AccountDto> getAccountsByUser(Integer id) {
+        log.info("Request for accounts by user id: {}...", id);
         return accountRepository.findAccountByUsersId(id).stream().map(accountMapper::mapToDto).collect(Collectors.toList());
     }
-    public Integer deactivateAccount(Integer id) {
 
+    public Integer deactivateAccount(Integer id) {
+        log.info("Request for deactivating account...");
+        log.trace("Verifying account of id: {} exists...", id);
         Optional<Account> deactivatedAccount = accountRepository.findById(id);
 
         Account account = deactivatedAccount.orElseThrow(AccountNotFoundException::new);
         if (!account.getActiveStatus()) throw new AccountNotFoundException();
         if (account.getBalance()!=0) throw new AccountNotEmptyException();
+
+        log.info("Deactivating account...");
         account.setActiveStatus(false);
         accountRepository.save(account);
+        log.info("Returning deactivated account id: {}", id);
         return account.getId();
     }
 
     public Integer updateAccount(UpdateAccountDto updateAccountDto, Integer id) {
+        log.info("Request for updating account id: {}...", id);
         try {
+            log.trace("Verifying account of id: {} exists...", id);
             Account account = accountRepository.getById(id);
             updateAccountMapper.updateEntity(updateAccountDto, account);
             List<User> users = new ArrayList<>();
             try {
+                log.trace("Verifying all users exist...");
+
                 updateAccountDto.getUsersIds().forEach(integer -> users.add(userRepository.findById(integer).orElseThrow(UserNotFoundException::new)));
+                log.trace("Assigning users to account...");
                 account.setUsers(users);
             } catch (NullPointerException e) {
+                log.warn("User not found...");
+                log.trace(e.getMessage());
                 throw new UserNotFoundException();
                 //no users provided in update
             }
+            log.info("Saving updated account information to database...");
             accountRepository.save(account);
+            log.info("Returning id of updated account...");
             return account.getId();
         } catch (EntityNotFoundException e) {
+            log.warn("Account not found...");
+            log.trace(e.getMessage());
             throw new AccountNotFoundException();
         } catch (DataIntegrityViolationException dive) {
+            log.warn("Account unable to be updated, data integrity violation...");
+            log.trace(dive.getMessage());
             Throwable thr = dive.getCause();
             log.info(dive.getMessage(), dive);
             throw dive;
@@ -97,20 +122,26 @@ public class AccountService {
     }
 
     public List<AccountDto> getAccounts() {
+        log.info("Finding all accounts, not paginated");
         return accountRepository.findAll().stream().map(accountMapper::mapToDto).collect(Collectors.toList());
     }
 
     public AccountDto getAccountById(Integer id) {
+        log.info("Finding account by ID");
         return accountMapper.mapToDto(accountRepository.findById(id).orElseThrow(AccountNotFoundException::new));
     }
 
     public Page<AccountDto> getAccounts(Integer pageNumber, Integer pageSize, boolean asc, String sort) {
+        log.info("Request for paginated results of all accounts...");
         Page<AccountDto> req;
         if (sort != null) {
+            log.info("Results sorted by {}...", sort);
             req = accountRepository.findAll(PageRequest.of(pageNumber, pageSize, Sort.by(asc ? Sort.Direction.ASC : Sort.Direction.DESC, sort))).map(account -> accountMapper.mapToDto(account));
         } else {
+            log.info("Results not sorted...");
             req = accountRepository.findAll(PageRequest.of(pageNumber, pageSize)).map(account -> accountMapper.mapToDto(account));
         }
+        log.info("Returning Page of AccountDto...");
         return req;
     }
 }
