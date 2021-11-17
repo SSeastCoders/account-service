6e1e8543129b020dab555800f39494e541c264e0
Hazel Baker-Harvey <84642569+hazelbakerharvey@users.noreply.github.com>
2021-11-17 04:27:22 -0800

    edited contstructor args

--- account-api/src/main/java/com/ss/eastcoderbank/accountapi/controller/AccountController.java
+++ account-api/src/main/java/com/ss/eastcoderbank/accountapi/controller/AccountController.java
@@ -7,6 +7,7 @@ import com.ss.eastcoderbank.core.transferdto.AccountDto;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -22,6 +23,7 @@ import java.util.List;
 @Slf4j
 @CrossOrigin
 @RestController
+@NoArgsConstructor
 @RequestMapping("api/v1/accounts")
 public class AccountController {
     @Autowired
